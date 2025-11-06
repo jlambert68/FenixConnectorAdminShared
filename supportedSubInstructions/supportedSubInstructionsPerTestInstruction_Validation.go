fc5a0c1af61de49140a496e9074a726367627642
jlambert68 <jonas.lambert@drakon.se>
2025-11-06 19:00:07 +0100

    Bugfix in code for validation ExecutionOrder for  SupportedSubInstructionsPerTestInstruction, update 2

--- supportedSubInstructions/supportedSubInstructionsPerTestInstruction_Validation.go
+++ supportedSubInstructions/supportedSubInstructionsPerTestInstruction_Validation.go
@@ -16,6 +16,7 @@ var embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema []byte
 // ValidateSupportedSubInstructionsPerTestInstruction
 // Validates the SupportedSubInstructionsPerTestInstruction
 func ValidateSupportedSubInstructionsPerTestInstruction(
+	supportedSubInstructionsJsonTAsByteArrayPtr *[]byte,
 	supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr *[][]byte) (err error) {
 
 	// Validates the SupportedSubInstructionsPerTestInstruction-json towards the json-schema
@@ -27,86 +28,13 @@ func ValidateSupportedSubInstructionsPerTestInstruction(
 
 	// Validates that the ExecutionOrder towards the PreConditions for SupportedSubInstructionsPerTestInstruction
 	err = validateExecutionOrderTowardsPreConditionsForSupportedSubInstructionsPerTestInstructionJson(
+		supportedSubInstructionsJsonTAsByteArrayPtr,
 		supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr)
 	if err != nil {
 		return err
 	}
 
 	return err
-}
-
-// Validates that the ExecutionOrder towards the PreConditions for SupportedSubInstructionsPerTestInstruction
-func validateExecutionOrderTowardsPreConditionsForSupportedSubInstructionsPerTestInstructionJson(
-	supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr *[][]byte) (
-	err error) {
-
-	// UnMarshal json-schema for SupportedSubInstructions
-	var catalog Catalog
-	if err = json.Unmarshal(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema, &catalog); err != nil {
-		common_config.Logger.WithFields(logrus.Fields{
-			"id":  "3ad2ad8c-4ce3-4a77-8240-62785d25c869",
-			"err": err,
-			"string(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema)": string(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema),
-		}).Error("Couldn't UnMarshal 'embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema' into struct used for processing ExecutionPlan")
-
-		return err
-	}
-
-	// Convert to object that can be validated
-	var supportedSubInstructionsPerTestInstructionToValidateAsByteArray [][]byte
-	supportedSubInstructionsPerTestInstructionToValidateAsByteArray = *supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr
-	var jsonObjectedToBeValidated interface{}
-
-	// Loop all SupportedSubInstructionsPerTestInstruction To be Validated
-	for _, tempSupportedSubInstructionsPerTestInstructionToValidated := range supportedSubInstructionsPerTestInstructionToValidateAsByteArray {
-
-		err = json.Unmarshal(tempSupportedSubInstructionsPerTestInstructionToValidated, &jsonObjectedToBeValidated)
-		if err != nil {
-			common_config.Logger.WithFields(logrus.Fields{
-				"id":  "69003008-a6aa-4fe4-a79c-e6d267d93aba",
-				"err": err,
-				"string(tempSupportedSubInstructionsPerTestInstructionToValidated)": string(tempSupportedSubInstructionsPerTestInstructionToValidated),
-			}).Error("Couldn't Unmarshal 'tempSupportedSubInstructionsPerTestInstructionToValidated'")
-
-			return err
-		}
-
-		// Validate that the 'supportedSubInstructionsJson' is valid towards the json-schema
-		// UnMarshal
-		var plan PlanRoot
-		if err = json.Unmarshal(tempSupportedSubInstructionsPerTestInstructionToValidated, &plan); err != nil {
-			common_config.Logger.WithFields(logrus.Fields{
-				"id":  "f95b0c2e-a0d1-437d-95ba-ebd2946821b9",
-				"err": err,
-				"tempSupportedSubInstructionsPerTestInstructionToValidated": string(tempSupportedSubInstructionsPerTestInstructionToValidated),
-			}).Error("Couldn't UnMarshal 'tempSupportedSubInstructionsPerTestInstructionToValidated' into struct used for processing ExecutionPlan")
-
-			return err
-		}
-
-		// --- Validate plan ---
-		result := ValidatePlan(catalog, plan)
-
-		// --- Print results ---
-		if !result.OK {
-
-		}
-		common_config.Logger.WithFields(logrus.Fields{
-			"id":  "25c1457d-6a6b-420d-9e4a-310a7e42fe00",
-			"err": result.Errors,
-			"tempSupportedSubInstructionsPerTestInstructionToValidated": string(tempSupportedSubInstructionsPerTestInstructionToValidated),
-		}).Error("Failed when validating ExecutionOrder")
-
-		err = errors.New("failed when validating PreConditions")
-
-		return err
-
-		return err
-
-	}
-
-	return err
-
 }
 
 // ValidateSupportedSubInstructionsPerTestInstructionJsonTowardsJsonSchema
@@ -180,6 +108,81 @@ func validateSupportedSubInstructionsPerTestInstructionJsonTowardsJsonSchema(
 
 			*/
 		}
+
+	}
+
+	return err
+
+}
+
+// Validates that the ExecutionOrder towards the PreConditions for SupportedSubInstructionsPerTestInstruction
+func validateExecutionOrderTowardsPreConditionsForSupportedSubInstructionsPerTestInstructionJson(
+	supportedSubInstructionsJsonTAsByteArrayPtr *[]byte,
+	supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr *[][]byte) (
+	err error) {
+
+	var supportedSubInstructionsAsCatalog Catalog
+	err = json.Unmarshal(*supportedSubInstructionsJsonTAsByteArrayPtr, &supportedSubInstructionsAsCatalog)
+	if err != nil {
+		common_config.Logger.WithFields(logrus.Fields{
+			"id":  "d80e6388-3977-4dd3-88f1-f038034d488b",
+			"err": err,
+			"string(*supportedSubInstructionsJsonTAsByteArrayPtr)": string(*supportedSubInstructionsJsonTAsByteArrayPtr),
+		}).Error("Couldn't UnMarshal 'supportedSubInstructionsJsonTAsByteArrayPtr' into struct used for processing ExecutionPlan")
+
+		return err
+	}
+
+	// UnMarshal json-schema for SupportedSubInstructionsPerTestInstruction
+	var supportedSubInstructionsPerTestInstructionJsonSchema PlanRoot
+	if err = json.Unmarshal(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema, &supportedSubInstructionsPerTestInstructionJsonSchema); err != nil {
+		common_config.Logger.WithFields(logrus.Fields{
+			"id":  "3ad2ad8c-4ce3-4a77-8240-62785d25c869",
+			"err": err,
+			"string(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema)": string(embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema),
+		}).Error("Couldn't UnMarshal 'embeddedFile_SupportedSubInstructionsPerTestInstructionJsonSchema' into struct used for processing ExecutionPlan")
+
+		return err
+	}
+
+	// Convert to object that can be validated
+	var supportedSubInstructionsPerTestInstructionToValidateAsByteArray [][]byte
+	supportedSubInstructionsPerTestInstructionToValidateAsByteArray = *supportedSubInstructionsPerTestInstructionToValidateAsByteArrayPtr
+
+	// Loop all SupportedSubInstructionsPerTestInstruction To be Validated
+	for _, tempSupportedSubInstructionsPerTestInstructionToValidated := range supportedSubInstructionsPerTestInstructionToValidateAsByteArray {
+
+		// Validate that the 'supportedSubInstructionsJson' is valid towards the json-schema
+		// UnMarshal
+		var supportedSubInstructionsPerTestInstructionToValidatedAsPlanRoot PlanRoot
+		if err = json.Unmarshal(tempSupportedSubInstructionsPerTestInstructionToValidated, &supportedSubInstructionsPerTestInstructionToValidatedAsPlanRoot); err != nil {
+			common_config.Logger.WithFields(logrus.Fields{
+				"id":  "f95b0c2e-a0d1-437d-95ba-ebd2946821b9",
+				"err": err,
+				"tempSupportedSubInstructionsPerTestInstructionToValidated": string(tempSupportedSubInstructionsPerTestInstructionToValidated),
+			}).Error("Couldn't UnMarshal 'tempSupportedSubInstructionsPerTestInstructionToValidated' into struct used for processing ExecutionPlan")
+
+			return err
+		}
+
+		// --- Validate supportedSubInstructionsPerTestInstructionToValidatedAsPlanRoot ---
+		result := ValidatePlan(supportedSubInstructionsAsCatalog, supportedSubInstructionsPerTestInstructionToValidatedAsPlanRoot)
+
+		// --- Print results ---
+		if !result.OK {
+
+		}
+		common_config.Logger.WithFields(logrus.Fields{
+			"id":  "8e6454cd-bb7c-4252-897f-e7291048c037",
+			"err": result.Errors,
+			"tempSupportedSubInstructionsPerTestInstructionToValidated": string(tempSupportedSubInstructionsPerTestInstructionToValidated),
+		}).Error("Failed when validating ExecutionOrder")
+
+		err = errors.New("failed when validating PreConditions")
+
+		return err
+
+		return err
 
 	}
 
